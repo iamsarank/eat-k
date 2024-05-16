b2924e8d7a0c3eaef5d9497a7cef38638f8d75ed
saravanakumar <iamsarank@gmail.com>
2024-05-16 13:56:54 +0530

    frontend completed

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Overall,"Creating Delicious Dishes" is an essential resource for anyone looking to start a food business or take their culinary skills to the next level.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
