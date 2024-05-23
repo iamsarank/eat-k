b42f1024172bd4bba5a22dddc4e7f87a5fc36daa
saravanakumar <iamsarank@gmail.com>
2024-05-23 13:59:26 +0530

    cart api completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import cors from 'cors'
+import 'dotenv/config'
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 //app config
@@ -19,6 +22,8 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
     res.send("API Working")
